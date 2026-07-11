a0d5672ca2caacce26a750b4ce095acd9ae99b84
xelvioraai-commits <xelvioraai@gmail.com>
2026-07-11 20:34:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Website JavaScript will be added here later.
+/* ==========================================
+   XEL ENGINE 2.0
+========================================== */
+
+const xel = document.getElementById("xel");
+
+window.addEventListener("load", initXel);
+
+function initXel(){
+
+    gsap.set(xel,{
+        x:window.innerWidth-120,
+        y:window.innerHeight-120
+    });
+
+    console.log("XEL READY");
+
+}
