da4e487e583fff0ec2f46c316c8e5f466ac1ed5a
xelvioraai-commits <xelvioraai@gmail.com>
2026-07-11 17:09:15 +0530

    Update xel.js

--- xel.js
+++ xel.js
@@ -1,1 +1,30 @@
-// XEL ENGINE V3
+/* ==========================================
+   XEL ENGINE V3
+========================================== */
+
+const xel = document.getElementById("xel");
+const bubble = document.getElementById("xel-message");
+
+const discoveryBtn = document.getElementById("discovery-btn");
+const heroTitle = document.getElementById("hero-title");
+
+/* ==========================================
+   START
+========================================== */
+
+window.addEventListener("load", startXel);
+
+function startXel(){
+
+    gsap.set(xel,{
+
+        x:0,
+        y:0,
+        scale:0.8,
+        rotation:0
+
+    });
+
+    console.log("XEL V3 READY");
+
+}
