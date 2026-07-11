2e4de170dd57286a736f9c455e0f318fa52a676e
xelvioraai-commits <xelvioraai@gmail.com>
2026-07-11 16:21:39 +0530

    Update xel.js

--- xel.js
+++ xel.js
@@ -1,3 +1,18 @@
-console.log("GSAP Loaded");
+/* ==========================================
+   XEL ENGINE v2
+========================================== */
 
-console.log(gsap);
+const xel = document.getElementById("xel");
+const xelImg = document.getElementById("xel-img");
+const bubble = document.getElementById("xel-message");
+
+const discoveryBtn = document.getElementById("discovery-btn");
+const heroTitle = document.getElementById("hero-title");
+
+window.addEventListener("load", startXel);
+
+function startXel(){
+
+    console.log("Starting Xel...");
+
+}
