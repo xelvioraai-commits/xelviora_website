c160f61033af355e02dbbd56c917bd452b49d83b
xelvioraai-commits <xelvioraai@gmail.com>
2026-07-14 21:11:16 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,7 +1,51 @@
-const continueBtn = document.getElementById("continueBtn");
+let currentStep = 1;
 
-continueBtn.addEventListener("click", () => {
+const button = document.getElementById("continueBtn");
 
-    alert("Step 2 Coming Next");
+const title = document.getElementById("formTitle");
 
-});
+const stepText = document.getElementById("stepText");
+
+button.onclick = function(){
+
+document.getElementById("step"+currentStep).classList.remove("active");
+
+currentStep++;
+
+if(currentStep>4){
+
+window.location.href="dashboard.html";
+
+return;
+
+}
+
+document.getElementById("step"+currentStep).classList.add("active");
+
+document.querySelector(".step.active").classList.remove("active");
+
+document.querySelectorAll(".step")[currentStep-1].classList.add("active");
+
+stepText.innerHTML="Step "+currentStep+" of 4";
+
+if(currentStep===2){
+
+title.innerHTML="Administrator Account";
+
+}
+
+if(currentStep===3){
+
+title.innerHTML="Workspace Configuration";
+
+}
+
+if(currentStep===4){
+
+title.innerHTML="Review & Create";
+
+button.innerHTML="Create Workspace";
+
+}
+
+};
