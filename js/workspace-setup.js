313ab31ccdee5419b6a94a0f5c2cd1c8107abd84
xelvioraai-commits <xelvioraai@gmail.com>
2026-07-15 10:31:05 +0530

    Update and rename unwanted to workspace-setup.js

---
+++ js/workspace-setup.js
@@ -0,0 +1,45 @@
+const progress=document.getElementById("progressBar");
+
+const status=document.getElementById("statusText");
+
+const steps=[
+
+"Creating Company...",
+
+"Creating Administrator...",
+
+"Configuring Workspace...",
+
+"Initializing AI...",
+
+"Preparing Dashboard..."
+
+];
+
+let width=0;
+
+let index=0;
+
+const timer=setInterval(()=>{
+
+width+=20;
+
+progress.style.width=width+"%";
+
+status.innerText=steps[index];
+
+index++;
+
+if(width>=100){
+
+clearInterval(timer);
+
+setTimeout(()=>{
+
+window.location.href="dashboard.html";
+
+},1200);
+
+}
+
+},1200);
