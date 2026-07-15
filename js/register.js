284c111aa77512ac1a0ab45f6753cc8d49b8980f
xelvioraai-commits <xelvioraai@gmail.com>
2026-07-15 08:54:39 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -49,3 +49,134 @@ button.innerHTML="Create Workspace";
 }
 
 };
+const reviewBtn = document.getElementById("reviewBtn");
+
+reviewBtn.onclick=function(){
+
+document.getElementById("reviewSection").style.display="block";
+
+document.getElementById("companyReview").innerHTML=`
+
+<div class="review-row">
+
+<span>Company Name</span>
+
+<span>${document.getElementById("companyName").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Business Email</span>
+
+<span>${document.getElementById("businessEmail").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Country</span>
+
+<span>${document.getElementById("country").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Industry</span>
+
+<span>${document.getElementById("industry").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Company Size</span>
+
+<span>${document.getElementById("companySize").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Business Phone</span>
+
+<span>${document.getElementById("phone").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Website</span>
+
+<span>${document.getElementById("website").value}</span>
+
+</div>
+
+`;
+
+document.getElementById("adminReview").innerHTML=`
+
+<div class="review-row">
+
+<span>Name</span>
+
+<span>${document.getElementById("adminName").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Job Title</span>
+
+<span>${document.getElementById("jobTitle").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Email</span>
+
+<span>${document.getElementById("adminEmail").value}</span>
+
+</div>
+
+`;
+
+document.getElementById("workspaceReview").innerHTML=`
+
+<div class="review-row">
+
+<span>Time Zone</span>
+
+<span>${document.getElementById("timezone").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Currency</span>
+
+<span>${document.getElementById("currency").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Language</span>
+
+<span>${document.getElementById("language").value}</span>
+
+</div>
+
+<div class="review-row">
+
+<span>Logo</span>
+
+<span>${document.getElementById("companyLogo").files.length ? document.getElementById("companyLogo").files[0].name : "No Logo Uploaded"}</span>
+
+</div>
+
+`;
+
+};
